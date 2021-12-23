73083fd1386f588645404847d8b70e0ae31ebc84
Kunanon <fronkzoza001@gmail.com>
2021-12-23 13:58:40 +0700

    Bug fix

--- Multi-Sig Wallet/MultiSig.sol
+++ Multi-Sig Wallet/MultiSig.sol
@@ -13,6 +13,10 @@ contract Wallet {
         uint id;
     }
     
+    event TransferRequestCreated(uint _id, uint _amount, address _initiator, address _receiver);
+    event ApprovalReceived(uint _id, uint _approvals, address _approver);
+    event TransferApproved(uint _id);
+
     Transfer[] transferRequests;
     
     mapping(address => mapping(uint => bool)) approvals;
@@ -20,8 +24,8 @@ contract Wallet {
     //Should only allow people in the owners list to continue the execution.
     modifier onlyOwners(){
         bool owner = false;
-        for(uint i=0;i<owners.length;i++){
-            if(msg.sender == owners[i]){
+        for(uint i=0; i<owners.length;i++){
+            if(owners[i] == msg.sender){
                 owner = true;
             }
         }
@@ -32,7 +36,6 @@ contract Wallet {
     constructor(address[] memory _owners, uint _limit) {
         owners = _owners;
         limit = _limit;
-
     }
     
     //Empty function
@@ -40,9 +43,11 @@ contract Wallet {
     
     //Create an instance of the Transfer struct and add it to the transferRequests array
     function createTransfer(uint _amount, address payable _receiver) public onlyOwners {
+        emit TransferRequestCreated(transferRequests.length, _amount, msg.sender, _receiver);
         transferRequests.push(
-            Transfer(_amount, _receiver,0,false,transferRequests.length)
-            );
+            Transfer(_amount, _receiver, 0, false, transferRequests.length)
+        );
+        
     }
     
     //Set your approval for one of the transfer requests.
@@ -53,14 +58,17 @@ contract Wallet {
     //An owner should not be able to vote on a tranfer request that has already been sent.
     function approve(uint _id) public onlyOwners {
         require(approvals[msg.sender][_id] == false);
-        require(transferRequests[msg.sender][_id].hasBeenSent == false);
-
+        require(transferRequests[_id].hasBeenSent == false);
+        
         approvals[msg.sender][_id] = true;
         transferRequests[_id].approvals++;
-
+        
+        emit ApprovalReceived(_id, transferRequests[_id].approvals, msg.sender);
+        
         if(transferRequests[_id].approvals >= limit){
             transferRequests[_id].hasBeenSent = true;
             transferRequests[_id].receiver.transfer(transferRequests[_id].amount);
+            emit TransferApproved(_id);
         }
     }
     
@@ -68,4 +76,6 @@ contract Wallet {
     function getTransferRequests() public view returns (Transfer[] memory){
         return transferRequests;
     }
+    
+    
 }
